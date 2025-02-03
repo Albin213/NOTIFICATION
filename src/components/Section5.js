893761a835c53e993102f78cefc0cb25237a64be
Albin <albinanto06@gmail.com>
2025-02-03 12:24:14 +0530

    update image name

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -4,8 +4,8 @@ function Section5() {
   return (
     <div className='px-4 sm:px-[30px] lg:px-[74px] py-10  drop-shadow-xl '>
     <div className='bg-white rounded-[34px] px-10 sm:px-24 xl:px-36 py-12 relative'>
-        <img src="vector 11.png" alt="" className='absolute right-0 top-0' />
-        <img src="vector 10.png" alt="" className='absolute left-0 bottom-0' />
+        <img src="vector11.png" alt="" className='absolute right-0 top-0' />
+        <img src="vector10.png" alt="" className='absolute left-0 bottom-0' />
 
         <h1 className=' text-[30px] md:text-[44px] text-[#05525F] font-normal pe-20 sm:pe-0 jomolhari-regular'>“GET IN TOUCH WITH OUR EXPERTS”</h1>
         <p className='text-[#1B1A1A]  text-[20px] sm:text-[26px] pt-8 sm:pt-14 pb-20 sm:pb-14 font-normal leading-10 poppins-regular sm:text-justify'>
